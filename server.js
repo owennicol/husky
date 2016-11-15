b03296d6e8448e1b57d37ec5a33b7a5648a0b199
Owen Nicol <owennicol@gmail.com>
2016-11-15 14:04:05 +0000

    More server changes

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use('/dist', express.static('dist'))
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
